0ebc23310b0473b899a60db23ce2139b633e9e16
AndreaDex <desiderio.andrea@outlook.it>
2021-05-19 16:08:28 +0200

    creato layout e completate funzionalita' di base

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,32 @@
-/* La nostra todo list avrá alcune tasks di default predefinite
-L'utente puó inserire nuove tasks
-Cliccando sulla "X" l'utente puó cancellare una task
-Se non ci sono piu task nella lista, mostrate un messaggio tipo "Nulla da fare"
-Inoltre L'utente vuole poter modificare una task giá inserita
-ma vuole anche poter indicare che la task é stata completata (con un icona cliccabile)
-Quando l'utente inserisce una task ha due modi per salvarla: o preme il pulsante add o preme il taso Enter della tastiera. Attenzione: l'utente non deve inserire tasks vuote ma almeno un tot di caratteri. */
+/*
+ *La nostra todo list avrá alcune tasks di default predefinite
+ * L'utente puó inserire nuove tasks
+ * Cliccando sulla "X" l'utente puó cancellare una task
+ * Se non ci sono piu task nella lista, mostrate un messaggio    tipo "Nulla da fare"
+ * Inoltre L'utente vuole poter modificare una task giá inserita
+ * ma vuole anche poter indicare che la task é stata completata (con un icona cliccabile)
+ * Quando l'utente inserisce una task ha due modi per salvarla: o preme il pulsante add o preme il taso Enter della tastiera. Attenzione: l'utente non deve inserire tasks vuote ma almeno un tot di caratteri. */
 
 const app = new Vue({
   el: "#app",
   data: {
+    minCharact: 5,
     newQuest: "",
-    journal: ["Quest 1", "Quest 2", "Quest 3"],
+    journal: ["Kill Kikimore", "Kill Gryphon", "Kill Leshen"],
   },
   methods: {
     addQuest() {
-      this.journal.push(this.newQuest);
-      this.newQuest = "";
+      if (this.newQuest.length >= this.minCharact) {
+        this.journal.unshift(this.newQuest);
+        this.newQuest = "";
+      } else {
+        alert("La quest deve avere almeno 5 caratteri");
+      }
     },
     removeQuest(index) {
+      this.journal.splice(index, 1);
+    },
+    completeQUest(index) {
       this.journal.splice(index, 1);
     },
   },
